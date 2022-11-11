e27e357e77ca17293383b722198ddc067e859e70
pamphilemkp <pamphilemkp@gmail.com>
2022-11-11 18:38:28 +0200

    add form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,36 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @user = User.find(params[:user_id])
   end
+
+  def new
+    @post = Post.new
+    @user = current_user
+    @id = params[:user_id]
+  end
+
+  def create
+    @user = current_user
+    @id = params[:user_id]
+    @post = Post.new(post_params)
+    respond_to do |format|
+      format.html do
+        if @post.save
+          # success message
+          # redirect to index
+          redirect_to "/users/#{@user.id}/posts/#{@post.id}"
+        else
+          # error message
+          # render new
+          render :new
+        end
+      end
+    end
+    
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
