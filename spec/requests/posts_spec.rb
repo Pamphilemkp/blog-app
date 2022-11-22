fc27a8574e507ff7423cc077a314684de1641577
pamphilemkp <pamphilemkp@gmail.com>
2022-11-22 21:10:37 +0200

    fix request sepc

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,18 +2,18 @@ require 'rails_helper'
 
 RSpec.describe 'Post', type: :request do
   it 'check user post index action' do
-    get '/users/:user_id/posts'
+    get '/users/1/posts'
 
     expect(response).to have_http_status(200)
     expect(response).to render_template(:index)
-    expect(response.body).to include('Welcome, here are different posts')
+    expect(response.body).to include('Number of posts')
   end
 
   it 'check user post show action' do
-    get '/user/:user_id/post/id'
+    get '/users/1/posts/1'
 
     expect(response).to have_http_status(200)
     expect(response).to render_template(:show)
-    expect(response.body).to include('Here is the post place')
+    expect(response.body).to include('Comments')
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,18 +2,18 @@ require 'rails_helper'
 
 RSpec.describe 'User', type: :request do
   it 'check user index action' do
-    get '/users/'
+    get users_path
 
     expect(response).to have_http_status(200)
     expect(response).to render_template(:index)
-    expect(response.body).to include('Hello, welcome to the blog')
+    expect(response.body).to include('Number of post')
   end
 
   it 'check user show action' do
-    get '/users/id'
+    get user_path(1)
 
     expect(response).to have_http_status(200)
     expect(response).to render_template(:show)
-    expect(response.body).to include('Here is where you can see more details about the user')
+    expect(response.body).to include('Bio')
   end
 end
