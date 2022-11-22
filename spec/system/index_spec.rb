85f0115e1bc78feac1222451258411cd16afb6f3
basitali111 <allahbadshah148@gmail.com>
2022-11-22 19:52:23 +0500

    Add test for users in index_spec.rb

--- spec/system/index_spec.rb
+++ spec/system/index_spec.rb
@@ -1,17 +1,27 @@
 require 'rails_helper'
-require "capybara/rspec"
+require 'capybara/rspec'
 
-RSpec.describe 'user index page', type: :system do
+RSpec.describe 'user index page', type: :feature do
+  before(:each) do
+    @user = User.create(name: 'Elon Musk', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'this is my bio')
 
- before(:each) do 
-  
-  @first_user = User.first
-  
-
+    visit users_path
   end
 
   it 'shows the right name of the first user' do
-    visit users_path
-      expect(page).to have_content('Tom')
+    expect(page).to have_content(@user.name)
+  end
+
+  it 'shows the right photo of the first user' do
+    expect(page.html).to include(@user.photo)
+  end
+
+  it 'shows the right posts of the first user' do
+    expect(page).to have_content(@user.posts_counter)
+  end
+
+  it 'redirects to a give user show page' do
+    click_link "See more about #{@user.name}"
+    expect(page).to have_current_path(user_path(@user.id))
   end
 end
