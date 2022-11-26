baca6d826aa939d25403ac104eb907212262df83
pamphilemkp <pamphilemkp@gmail.com>
2022-11-26 18:05:19 +0200

    add api end point

---
+++ app/controllers/api/v1/comments_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::CommentsController < ApplicationController
+
+  def index
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments
+
+    render json: @comment
+  end
+
+   def create 
+    @post = Post.find(params[:post_id])
+    
+    return unless @post.comments.create(text: params[:comment][:text], author: @current_user)
+    render json: { success: 'Comment created successfully' }
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,22 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [] do
+        resources :posts, only: [:index, :show] do
+          resources :comments, only: [:index, :create]
+        end
+      end
+    end
+  end
+
+
   # Defines the root path route ("/")
    root "users#index"
 
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create, :destroy] do
-      
-    end
+    resources :posts, only: [:index, :show, :new, :create, :destroy]
   end
 
   
