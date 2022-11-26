3eb2811848dcd34e6331b838e983dcb053c522fa
basitali111 <allahbadshah148@gmail.com>
2022-11-26 22:04:29 +0500

    Add rswag configuration

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::PostsController < ApplicationController
-    def index
-        @posts = Post.where(author_id: params[:user_id]).includes(:comments) 
-        render json: @posts
-      end
-    
-      def show
-        @post = Post.find(params[:id])
-        render json: @post
-      end
+  def index
+    @posts = Post.where(author_id: params[:user_id]).includes(:comments)
+    render json: @posts
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    render json: @post
+  end
 end
